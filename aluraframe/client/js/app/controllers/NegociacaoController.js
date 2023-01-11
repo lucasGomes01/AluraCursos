633697d0d618e7df3d1bafb9f6f8d1c768064b50
Lucas José de Lima Gomes <gomes.lucasj@gmail.com>
2023-01-10 22:59:43 -0300

    continuação aluraFrame
    aula 2

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,20 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this.inputDate = $('#date');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let $ = document.querySelector.bind(document);
+        let inputDate = this.inputDate
+        let inputQuantidade = this.inputQuantidade
+        let inputValor = this.inputValor
+
+
+    }
+}
