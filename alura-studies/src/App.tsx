dd39d5ad3ac52314cdce67aba0b1036553a9669d
Lucas José de Lima Gomes <gomes.lucasj@gmail.com>
2023-01-22 22:33:48 -0300

    sincronizando pastas
    adicionar alterações que foram feitas em outra maquina através do pendrive

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from './components/Botao/Formulario';
+import Lista from './components/Botao/Lista';
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+      < Lista />
+    </div>
+  );
+}
+
+export default App;
