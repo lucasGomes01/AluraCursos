be4555937ed81ebed4f32c51ed925667439a86b1
Lucas José de Lima Gomes <gomes.lucasj@gmail.com>
2023-01-22 23:32:48 -0300

    aluraStudies - aula 3

--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Botao from "../Botao";
+import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
     render () {
         return (
-            <form>
-                <div>
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
                     <input id="tarefa" name="tarefa" placeholder="O que você quer estudar" type="text" required />
                 </div>
-                <div>
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
--- alura-studies/src/components/Lista/index.tsx
+++ alura-studies/src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './Lista.module.scss'
 
 function Lista() {
     const tarefas =[{
@@ -14,11 +15,11 @@ function Lista() {
         tempo: '03:00:00'
     }]
     return (
-        <aside>
+        <aside className={style.listaTarefa}>
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index}>
+                    <li key={index} className={style.item}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
