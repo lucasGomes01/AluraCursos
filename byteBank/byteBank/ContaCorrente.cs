e6378d914624a63aa334b8936eaf11237f62296b
Lucas José de Lima Gomes <gomes.lucasj@gmail.com>
2023-01-24 22:31:53 -0300

    byteBank - aula 2

--- byteBank/byteBank/ContaCorrente.cs
+++ byteBank/byteBank/ContaCorrente.cs
@@ -12,5 +12,33 @@ namespace byteBank
         public string conta;
         public string titular;
         public double saldo;
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (this.saldo < valor)
+                return false;
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
     }
 }
--- byteBank/byteBank/Program.cs
+++ byteBank/byteBank/Program.cs
@@ -1,11 +1,6 @@
 ﻿using byteBank;
 
-ContaCorrente contaDoAndre = new ContaCorrente();
-contaDoAndre.titular = "André Silva";
-contaDoAndre.numero_agencia = 15;
-contaDoAndre.conta = "1010-x";
-contaDoAndre.saldo = 15.123000;
-
+#region Pratica dos desafios
 Produto produto = new();
 produto.Valor = 1000;
 produto.Nome = "maça";
@@ -13,5 +8,41 @@ produto.Fabricante = "";
 produto.Quantidade = 1000;
 produto.CodigoBarras = 145781324;
 
+//Console.WriteLine("Produto criado do exercício é = " + produto.Nome);
+#endregion
+
+#region Codigos que seriam apagados
+//contaDoAndre.Depositar(123);
+//Console.WriteLine("Saldo da conta do Andre = " + contaDoAndre.saldo);
+
+//if (contaDoAndre.Sacar(300))
+//    Console.WriteLine("Saldo da conta do Andre = " + contaDoAndre.saldo);
+//else
+//    Console.WriteLine("Saldo insuficiente para saque");
+#endregion
+
+ContaCorrente contaDoAndre = new ContaCorrente
+{
+    titular = "André Silva",
+    numero_agencia = 15,
+    conta = "1010-x",
+    saldo = 200
+};
+
 Console.WriteLine("Saldo da conta do Andre = " + contaDoAndre.saldo);
-Console.WriteLine("Produto criado do exercício é = " + produto.Nome);
+
+ContaCorrente contaDaMaria = new()
+{
+    titular = "Maria Souza",
+    numero_agencia = 17,
+    conta = "1010-5",
+    saldo = 350
+};
+
+Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+Console.WriteLine("---------------------------------------");
+
+contaDoAndre.Transferir(50, contaDaMaria);
+
+Console.WriteLine("Saldo da conta do Andre = " + contaDoAndre.saldo);
+Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
