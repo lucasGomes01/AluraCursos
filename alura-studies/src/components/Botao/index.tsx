31a3d869b993d1c588003f1f7cf4d6d0d78c488a
Lucas José de Lima Gomes <gomes.lucasj@gmail.com>
2023-02-05 10:17:20 -0300

    aluraStudies - aula 5
    - Nova renderização de um componente;
    - Criar estados com function components e class components;
    - Controlar campos de um Formulário com estados;
    - Criar interfaces e reutilizá-las;
    - Utilizar props para informar uma mudança de estado;

--- alura-studies/src/components/Botao/index.tsx
+++ alura-studies/src/components/Botao/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from './Botao.module.scss'
 
-class Botao extends React.Component<{children?: React.ReactNode}> {
+class Botao extends React.Component<{type?: "button" | "submit" | "reset" | undefined, children?: React.ReactNode}> {
     render() {
+        const { type = "button" } = this.props;
         return (
-            <button className={style.botao}>
+            <button type={type} className={style.botao}>
                 {this.props.children}
             </button>
         )
--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -1,24 +1,41 @@
 import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from './Formulario.module.scss'
+import { v4 as uuidv4 } from 'uuid';
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}> {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
 
-class Formulario extends React.Component {
+    adicionarTarefa(evento: React.FormEvent) {
+        evento.preventDefault();
+        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, { ...this.state, selecionado: false, completado: false, id: uuidv4() }]);
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
+    }
+
     render () {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
-                    <input id="tarefa" name="tarefa" placeholder="O que você quer estudar" type="text" required />
+                    <input id="tarefa" value={this.state.tarefa} onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value})} name="tarefa" placeholder="O que você quer estudar" type="text" required />
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
-                    <input id="tempo" max="01:30:00" min="00:00:00" name="tempo" step="1" type="time" required />
+                    <input id="tempo" value= {this.state.tempo} onChange={evento => this.setState({...this.state, tempo: evento.target.value })} max="01:30:00" min="00:00:00" name="tempo" step="1" type="time" required />
                 </div>
-                <Botao> Adicionar </Botao>
+                <Botao type="submit"> Adicionar </Botao>
             </form>
         )
     }
