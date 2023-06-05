35cb2569cad18ab6770a8853eff2a54eacb97d8c
Lucas José de Lima Gomes <gomes.lucasj@gmail.com>
2023-06-05 14:02:58 -0300

    csharp_exception Aula 5
    - Uso do bloco finally;
    - IOException nos permite trabalhar com exceções geradas ao acessar informações usando fluxos, arquivos e diretórios;

---
+++ byteBank entendendo exceções/csharp_exception/LeitorDeArquivo.cs
@@ -0,0 +1,28 @@
+﻿namespace ByteBank
+{
+    public class LeitorDeArquivo //: IDisposable
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            // throw new FileNotFoundException();
+
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha. . .");
+            throw new IOException();
+            return "Linha do arquivo";
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
--- byteBank entendendo exceções/csharp_exception/Program.cs
+++ byteBank entendendo exceções/csharp_exception/Program.cs
@@ -1,21 +1,39 @@
-﻿using csharp_exception;
+﻿using ByteBank;
+using csharp_exception;
 using csharp_exception.Contas;
+
+
+LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt");
 
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
-    //conta1.Sacar(110);
-    //Console.WriteLine(conta1.GetSaldo());
+    leitor.LerProximaLinha();
+    leitor.LerProximaLinha();
 }
-catch(ArgumentException ex)
+catch
 {
-    Console.WriteLine("Parâmetro com erro: " + ex.ParamName);
-    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
-    Console.WriteLine(ex.StackTrace);
-    Console.WriteLine(ex.Message);    
+    Console.WriteLine("Leitura de arquivo interrompida");
 }
-catch (SaldoInsuficienteException ex) 
+finally
 {
-    Console.WriteLine("Operação negada! Saldo Insuficiente!");
-    Console.WriteLine(ex.Message);
+    leitor.Fechar();
 }
+
+//try
+//{
+//    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+//    //conta1.Sacar(110);
+//    //Console.WriteLine(conta1.GetSaldo());
+//}
+//catch(ArgumentException ex)
+//{
+//    Console.WriteLine("Parâmetro com erro: " + ex.ParamName);
+//    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+//    Console.WriteLine(ex.StackTrace);
+//    Console.WriteLine(ex.Message);    
+//}
+//catch (SaldoInsuficienteException ex) 
+//{
+//    Console.WriteLine("Operação negada! Saldo Insuficiente!");
+//    Console.WriteLine(ex.Message);
+//}
