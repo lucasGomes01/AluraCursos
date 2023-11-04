07394bea4c22437f97150a239ef872f71d9809c0
Lucas José de Lima Gomes <gomes.lucasj@gmail.com>
2023-11-04 01:26:33 -0300

    aluraplay - aula 1 e 2

---
+++ aluraplay/js/conectaApi.js
@@ -0,0 +1,29 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function criarVideo(){
+    const conexao = await fetch("http://localhost:3000/videos" ,{
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.joson();
+    return  conexaoConvertida;
+}
+
+export const conectaApi = {
+    listaVideos,
+    criarVideo
+}
